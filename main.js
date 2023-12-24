e0e37680e584bca22992faa3e47bf771cda5a9c1
Michael Zhang <michaelzhang@mz-M1.local>
2023-12-23 22:42:16 -0800

    added scoring logic for first player

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import "./scss/style.scss";
+
+const plusButton = document.querySelector(".score-controls .add");
+console.log(plusButton);
+
+const minusButton = document.querySelector(".score-controls .minus");
+console.log(minusButton);
+
+var score = document.querySelector("#score").firstChild.nodeValue;
+
+function addScore() {
+	score = parseInt(score) + 100;
+	document.querySelector("#score").innerHTML = score;
+}
+
+function minusScore() {
+	score = parseInt(score) - 100;
+	document.querySelector("#score").innerHTML = score;
+}
+
+plusButton.addEventListener("click", addScore);
+minusButton.addEventListener("click", minusScore);
