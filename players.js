dd78e98e71057378485ce7d3f785d929c812a9ce
Michael Zhang <michaelzhang@mz-M1.local>
2023-12-30 01:02:17 -0800

    added logic and basic ui for adding/removing players

--- players.js
+++ players.js
@@ -7,66 +7,23 @@ var teams = [
 ];
 
 export function makePlayers() {
-	const plusButton = document.querySelector(".score-controls .add");
-	//console.log(plusButton);
-
-	const minusButton = document.querySelector(".score-controls .minus");
-	//console.log(minusButton);
-
 	const footer = document.querySelector(".grid__footer");
 	//console.log(footer);
 
 	teams.forEach((t) => {
-		//console.log(t.teamName);
-		// Add parent div
-		const playerDiv = document.createElement("div");
-		playerDiv.classList.add("player");
-		footer.append(playerDiv);
-
-		// Team name
-		const team = document.createElement("p");
-		team.classList.add("teamName");
-		team.setAttribute("contenteditable", "true");
-		playerDiv.append(team);
-		team.innerText = t.teamName;
-
-		// Score
-		const scoreHTML = document.createElement("p");
-		scoreHTML.classList.add("score-controls");
-		scoreHTML.setAttribute("id", "score");
-		playerDiv.append(scoreHTML);
-		scoreHTML.innerText = t.score;
-
-		// Score controls (+ / -)
-		const tmpDiv = document.createElement("div");
-		playerDiv.append(tmpDiv);
-
-		const addBtn = document.createElement("button");
-		addBtn.classList.add("add");
-		const minusBtn = document.createElement("button");
-		minusBtn.classList.add("minus");
-		tmpDiv.append(addBtn);
-		tmpDiv.append(minusBtn);
-
-		const btnPlus = document.createElement("p");
-		btnPlus.innerText = "+";
-		const btnMinus = document.createElement("p");
-		btnMinus.innerText = "-";
-		addBtn.append(btnPlus);
-		minusBtn.append(btnMinus);
-
-		//console.log(scoreHTML);
-		addBtn.addEventListener("click", function () {
-			var incr = getGlobalPoints();
-			t.score += incr;
-			updateScore();
-		});
-		minusBtn.addEventListener("click", function () {
-			var incr = getGlobalPoints() * -1;
-			t.score += incr;
-			updateScore();
-		});
+		createOnePlayer(t, footer);
 	});
+
+	const addPlayerBtn = document.createElement("button");
+	addPlayerBtn.setAttribute("id", "add-new-player");
+	addPlayerBtn.innerText = "add";
+	footer.append(addPlayerBtn);
+	addPlayerBtn.addEventListener("click", () => addPlayer());
+
+	const removePlayerBtn = document.createElement("button");
+	removePlayerBtn.innerText = "remove";
+	footer.append(removePlayerBtn);
+	removePlayerBtn.addEventListener("click", () => removeLastPlayer());
 }
 
 export function updateScore() {
@@ -82,4 +39,77 @@ export function updateScore() {
 	for (let i = 0; i < scoreElements.length; i++) {
 		scoreElements[i].innerText = scoresArray[i];
 	}
+}
+
+function addPlayer() {
+	const footer = document.querySelector(".grid__footer");
+	var newPlayer = { teamName: "New Team", score: 0 };
+	teams.push(newPlayer);
+	createOnePlayer(newPlayer, footer);
+	console.log(teams);
+}
+
+function removeLastPlayer() {
+	if (teams.length <= 1) return;
+	const footer = document.querySelector(".grid__footer");
+	const allPlayers = footer.querySelectorAll(".player");
+	const lastPlayer = allPlayers[allPlayers.length - 1];
+	lastPlayer.remove();
+	teams.pop();
+}
+
+function createOnePlayer(t, footer) {
+	//console.log(t.teamName);
+	const addNewPlayerBtn = document.getElementById("add-new-player");
+	// Add parent div
+	const playerDiv = document.createElement("div");
+	playerDiv.classList.add("player");
+	footer.insertBefore(playerDiv, addNewPlayerBtn);
+
+	// Team name
+	const team = document.createElement("p");
+	team.classList.add("teamName");
+	team.setAttribute("contenteditable", "true");
+	playerDiv.append(team);
+	team.innerText = t.teamName;
+
+	// Score
+	const scoreHTML = document.createElement("p");
+	scoreHTML.classList.add("score-controls");
+	scoreHTML.setAttribute("id", "score");
+	playerDiv.append(scoreHTML);
+	scoreHTML.innerText = t.score;
+
+	// Score controls (+ / -)
+	const tmpDiv = document.createElement("div");
+	playerDiv.append(tmpDiv);
+
+	// const plusButton = document.querySelector(".score-controls .add");
+	// const minusButton = document.querySelector(".score-controls .minus");
+
+	const addBtn = document.createElement("button");
+	addBtn.classList.add("add");
+	const minusBtn = document.createElement("button");
+	minusBtn.classList.add("minus");
+	tmpDiv.append(addBtn);
+	tmpDiv.append(minusBtn);
+
+	const btnPlus = document.createElement("p");
+	btnPlus.innerText = "+";
+	const btnMinus = document.createElement("p");
+	btnMinus.innerText = "-";
+	addBtn.append(btnPlus);
+	minusBtn.append(btnMinus);
+
+	//console.log(scoreHTML);
+	addBtn.addEventListener("click", function () {
+		var incr = getGlobalPoints();
+		t.score += incr;
+		updateScore();
+	});
+	minusBtn.addEventListener("click", function () {
+		var incr = getGlobalPoints() * -1;
+		t.score += incr;
+		updateScore();
+	});
 }
